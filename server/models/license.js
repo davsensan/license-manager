6a7a263acbb0e58cb22a651c457b51a7c288682d
David Señas Sanvicente <davsensan@gmail.com>
2017-07-10 18:41:40 +0200

    Añadido modelo para licencias

---
+++ server/models/license.js
@@ -0,0 +1,81 @@
+const DataTypes = require('sequelize/lib/data-types');
+
+module.exports = function(sequelize) {
+  const License = sequelize.define('Licenses',
+      { 
+        id:{
+            type : DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+            validate: {
+                isUUID: 4
+            }            
+        },
+        license_uuid : {
+            type : DataTypes.UUID,
+            allowNull: false,
+            defaultValue: DataTypes.UUIDV4,
+            validate : {
+                notEmpty: {
+                    msg: "Field licenses uuid shouldn't be empty"
+                }
+            }
+        },
+        organization_uuid : {
+            type : DataTypes.UUID
+            }
+        },
+        duration: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            validate: {
+                notEmpty: {
+                    msg: "Field duration shouldn't be empty"
+                }
+            }
+        },
+        expires_at: {
+            type: DataTypes.DATE,
+        },
+        sensors : {
+            type: DataTypes.JSON,
+            allowNull: false,
+            validate:{
+                notEmpty: {msg: "Field sensors shouldn't be empty"}
+            }
+        },
+        limit_bytes : {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate : {
+                notEmpty: {
+                    msg: "Field limit bytes shouldn't be empty"
+                }
+            }
+        },
+        enabled : {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+            validate : {
+                notEmpty: {
+                    msg: "Field enabled shouldn't be empty"
+                }
+            }
+        },
+        delete : {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+            validate : {
+                notEmpty: {
+                    msg: "Field delete shouldn't be empty"
+                }
+            }
+        },
+        comments : {
+            type: DataTypes.STRING
+        }    
+    });
+    return License;
+}
